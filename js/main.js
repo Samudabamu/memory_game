0e9af1c94dabdaeeb1ab5a871c255cb9947fb966
Samu <Samuel.westen01@gmail.com>
2017-12-03 16:32:18 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
+
+
+checkForMatch = function () {
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} 
+	else {
+	alert("Sorry, try again.");
+	}
+  }
+}
+
+var flipCard = function (cardId) {
+
+/* 
 var cardOne = cards[0];
 cardsInPlay.push(cardOne); 
 console.log("User flipped " + cardOne);
@@ -9,7 +25,16 @@ console.log("User flipped " + cardOne);
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+*/
+console.log ("User flipped " + cards[cardId]);
 
+cardsInPlay.push(cards[cardId]); 
+
+checkForMatch();
+
+
+
+/*
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -17,6 +42,9 @@ if (cardsInPlay.length === 2) {
 	else {
 		alert("Please try again.");
 	}
-}
+  }
+*/
+} 
 
-
+flipCard(0);
+flipCard(2);
